0e69756f7b4302e3d48ae57825bfc42db37ad18a
Ignas Vyšniauskas <baliulia@gmail.com>
2014-06-28 21:27:20 +0200

    Non-leaking lxml with XPath queries

--- lxml_p.py
+++ lxml_p.py
@@ -4,11 +4,16 @@ import sys
 from lxml import etree
 
 def get_producturls(f):
-    parser = etree.iterparse(f, tag='ProductUrl')
-
-    for _, element in parser:
-        print (element.text)
-        element.clear()
+    context = etree.iterparse(f, events=('end',))
+    query1 = etree.XPath("string(concat('wap: ', ProductUrl))", smart_strings=False)
+    query2 = etree.XPath("string(ProductUrl)", smart_strings=False)
+    for _, element in context:
+        if element.tag=='Product':
+            print (query1(element))
+            print (query2(element))
+            element.clear()
+            while element.getprevious() is not None:
+                del element.getparent()[0]
 
 
 def entry_point(argv):
